a73e9885548f937e149905a249e117064a5c4aae
geheimerr <tejas.shandilya24@gmail.com>
2025-12-25 22:58:40 +0530

    Add helper functions and implement Shannon Entropy

--- stats.py
+++ stats.py
@@ -1,3 +1,19 @@
+import re
+import math
+from collections import Counter
+
+# helper functions
+
+def get_words(text):
+# cleans and tokenizes words from the text"
+    text = re.sub(r'[^\w\s]', '', text)
+
+def get_sentences(text):
+    # Split by period, exclamation, or question mark
+    raw_sentences = re.split(r'[.!?]+', text)
+    # Filter out empty strings and strip whitespace
+    return [s.strip() for s in raw_sentences if s.strip()]
+
 """
 this function asks the user for a file path and reads the
 file at the destination, returning its content as a string
@@ -40,3 +56,20 @@ def sort_chars(char_dict):
 
     char_list.sort(reverse=True, key=get_num)
     return char_list
+
+'''
+Function to calculate the Shannon Entropy of the text. 
+A higher score would mean the text is more complex and a lower score would mean that the text is simpler to comprhehnd.
+'''
+def calc_entropy(text):
+    if not text: return 0
+    prob_dist = Counter(text)
+    total_len = len(text)
+    entropy = 0
+    for count in prob_dist.values():
+        p = count / total_len
+        entropy -= p * math.log2(p)
+    return entropy
+
+
+
